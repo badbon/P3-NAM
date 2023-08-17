e5530cea5dd9bf8cf8d5167e29fa99187c9113aa
Nick <ndplay@ymail.com>
2023-08-17 15:20:21 +0400

    Mapgen/grid fix

--- Assets/JungleGenerator.cs
+++ Assets/JungleGenerator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,41 +6,61 @@ public class JungleGenerator : MonoBehaviour
     public Tilemap tilemap;
     public Tile grassTile;
     public Tile waterTile;
-    public GameObject treePrefab;  // Use a GameObject prefab instead of a Tile for trees
-    //... Add more tiles as needed
+    public GameObject treePrefab;
 
     public Vector3Int mapSize = new Vector3Int(100, 100, 1);
-    public float noiseScale = 0.1f;
+    public float treeSpawnChance = 0.02f;  // 2% chance for a tree to spawn on a grass tile
 
-    void Start()
+    private void Start()
     {
-        GenerateMap();
+        FillWithGrass();  // Fill the entire map with grass tiles
+        PlaceWaterTiles();  // Use Perlin noise to decide where to place some water tiles
+        PlaceTrees();  // Randomly place trees on grass tiles
     }
 
-    void GenerateMap()
+    private void FillWithGrass()
     {
         for (int x = 0; x < mapSize.x; x++)
         {
             for (int y = 0; y < mapSize.y; y++)
             {
                 Vector3Int cellPosition = new Vector3Int(x, y, 0);
-                float noiseValue = Mathf.PerlinNoise(x * noiseScale, y * noiseScale);
+                Debug.Log(cellPosition);
+                tilemap.SetTile(cellPosition, grassTile);
+            }
+        }
+    }
 
-                if (noiseValue < 0.3f)
+    private void PlaceWaterTiles()
+    {
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                float noiseValue = Mathf.PerlinNoise(x * 0.05f, y * 0.05f);
+
+                // If noiseValue is less than 0.1, place a water tile over the grass tile
+                if (noiseValue < 0.1f)
                 {
                     tilemap.SetTile(cellPosition, waterTile);
-                }
-                else if (noiseValue < 0.5f)
-                {
-                    tilemap.SetTile(cellPosition, grassTile);
-                    Instantiate(treePrefab, new Vector3(x, y, 0), Quaternion.identity, this.transform);
-                }
-                else
-                {
-                    tilemap.SetTile(cellPosition, grassTile);
                 }
             }
         }
     }
 
+    private void PlaceTrees()
+    {
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                if (tilemap.GetTile(cellPosition) == grassTile && Random.value < treeSpawnChance)
+                {
+                    Instantiate(treePrefab, new Vector3(x + 0.5f, y + 0.5f, 0), Quaternion.identity, this.transform);
+                }
+            }
+        }
+    }
 }
