9030bd34c345837bcaa4ac7a4cd8b6a63553b15a
Nick <ndplay@ymail.com>
2023-09-18 07:07:10 +0400

    Enemy patrol

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,31 +1,80 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
-    public float health = 100f; // 100/100
-    private float maxHealth = 100f; // Start health
+    public JungleGenerator mapGenerator;
+    private Vector2 currentTarget;
+
+    public float health = 100f;
+    private float maxHealth = 100f;
     public float speed = 10.0f;
+    public float detectionRange = 5.0f;
+    public float reachedTargetThreshold = 0.5f; // Distance to consider that NPC reached its target
+
+    private Transform player;
 
     public Loadout loadout;
-    
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Assuming your player object has the tag "Player"
+        ChooseNewTarget();
     }
 
     void Update()
     {
-        
+        Patrol();
+        DetectPlayer();
+    }
+
+    private void Patrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, currentTarget) < reachedTargetThreshold)
+        {
+            ChooseNewTarget();
+        }
+    }
+
+    private void ChooseNewTarget()
+    {
+        Vector3Int randomCell;
+
+        do
+        {
+            randomCell = new Vector3Int(
+                Random.Range(0, mapGenerator.mapSize.x),
+                Random.Range(0, mapGenerator.mapSize.y),
+                0
+            );
+        }
+        while (mapGenerator.tilemap.GetTile(randomCell) == mapGenerator.waterTile);
+
+        currentTarget = mapGenerator.tilemap.GetCellCenterWorld(randomCell);
+    }
+
+    private void DetectPlayer()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        // Put your code for attacking the player here
+        Debug.Log("Attacking Player");
     }
 
     internal void TakeDamage(float damage)
     {
         Debug.Log("NPC took " + damage + " damage");
         health -= damage;
-        if(health <= 0)
+        if (health <= 0)
         {
             Die();
         }
@@ -35,6 +84,5 @@ public class NPCController : MonoBehaviour
     {
         Debug.Log("NPC died");
         Destroy(gameObject);
-
     }
 }
